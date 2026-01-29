424a881eb4e67525d397d45406847977a34d1d30
Lucas Chin <chinlucas4609@gmail.com>
2026-01-28 21:07:16 -0500

    Skeleton Spawning

--- pillar_spawner.gd
+++ pillar_spawner.gd
@@ -1,6 +1,7 @@
 extends StaticBody3D
 
 @onready var timer = $Timer
+const skeleton = preload("res://models/skeleton/skeleton.tscn")
 
 var radius = 15.0
 
@@ -11,5 +12,17 @@ func _on_timer_timeout() -> void:
 	spawn()
 
 func spawn():
-	print("spawn")
+	var skel = skeleton.instantiate()
+	var rand = randi() % 2
+	if rand == 1:
+		skel.position.x = radius * randf() 
+	else:
+		skel.position.z = radius * randf()
+		
+	rand = randi() % 2
+	if rand == 1:
+		skel.position = skel.position * Vector3(-1, -1, -1)
+	
+		
+	add_sibling(skel)
 	
